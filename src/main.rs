0b0e872d225582c8ef19d9017d45523402ce02ec
Emil <emilbabazade29@gmail.com>
2020-06-01 01:17:33 +0300

    working bare minimum bootimage

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 #![no_std]
 #![no_main]
 
-// starting point
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+use core::panic::PanicInfo;
+
+// this function is called during panic
+#[panic_handler]
+fn panic(_panic: &PanicInfo) -> ! {
     loop {}
 }
 
-use core::panic::PanicInfo;
+// starting point
+static NICE: &[u8] = b"Badamdar yek";
 
-#[panic_handler]
-fn panic(_panic: &PanicInfo) -> ! {
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for(i, &byte) in NICE.iter().enumerate() {
+        unsafe{
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    
     loop {}
 }
